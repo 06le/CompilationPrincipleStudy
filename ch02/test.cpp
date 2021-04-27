594fb2a5e93c12f05d71b66c349ac4d02042a294
06le <2966471989@qq.com>
2021-04-27 11:26:03 +0800

    Create test.cpp

---
+++ ch02/test.cpp
@@ -0,0 +1,17 @@
+//ch02 test
+#include "TranslatorForExpressions.h"
+//#include "PredictiveParsing.h"
+
+void test3()
+{
+	Parser p(cin);
+	p.expr();
+}
+
+int main()
+{
+	test3();
+	return 0;
+}
+
+
